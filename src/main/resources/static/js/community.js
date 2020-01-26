1439ef288c8c0dfdc701328b008f4c56c8a9423f
AngusZHAN <573585136@qq.com>
2020-01-26 14:40:30 +0800

    improved comment function

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -14,9 +14,16 @@ function post() {
             if (response.code == 200) {
                 $("#comment-section").hidden();
             } else {
-                alert(response.message);
+                if (response == 2003) {
+                    var isAccepted = confirm(response.message);
+                    if (isAccepted) {
+                        window.open("https://github.com/login/oauth/authorize?client_id=a384df0d388b6611bfda&redirect_uri=http://localhost:9517/callback&scope=user&state=1")
+                        window.localStorage.setItem("closable", true);
+                    }
+                } else {
+                    alert(response.message);
+                }
             }
-            console.log(response)
         },
         dataType: "json"
     });
